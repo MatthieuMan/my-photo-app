44b4acb97fdfb9460bb1b3579a03d1bdd1dd4f77
Matthieu Mandoula <mmandoula@hbs-research.com>
2019-02-14 17:48:25 +0100

    submission process

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,24 @@
         <h1>My Photo App</h1>
       </div>
       <!-- PhoneBody Component -->
-      <!-- pass in the posts and filters data arrays as props, shorthands will be ":posts" -->
-      <Phonebody v-bind:postsFromUppermostComponent="posts" v-bind:filtersFromUppermostComponent="filters"  />
+      <!-- bind data as props, shorthands will be ":posts" -->
+      <Phonebody v-bind:step="step"
+                 v-bind:postsFromUppermostComponent="posts"
+                 v-bind:filtersFromUppermostComponent="filters"
+                 :image="image"
+                 :selectedFilter="selectedFilter"
+                 v-model="caption"/>
       <!-- Phonebody Component -->
       <div class="phone-footer">
         <div class="home-cta">
           <i class="fas fa-home fa-lg"></i>
         </div>
         <div class="upload-cta">
-          <i class="far fa-plus-square fa-lg"></i>
+          <input type="file" name="file" id="file" class="inputfile"
+                 @change="uploadImage"/><!--hold the responsibility of uploading the image and then directing the user to step 2.-->
+          <label for="file">
+            <i class="far fa-plus-square fa-lg"></i>
+          </label>
         </div>
       </div>
     </div>
@@ -31,9 +40,31 @@
         name: "App",
         data() { // centralize all data in the uppermost parent App component. Children components will be more presentational and simply have their data as props passed in from App.
             return {
+                step: 1,//change the UI depending on where the user is on the submission process
                 posts,
                 filters,
+                image: "",
+                selectedFilter: "",
+                caption: ""
             };
+        },
+        methods: {
+            uploadImage(evt) {
+                const files = evt.target.files;
+                if (!files.length) return;
+                const reader = new FileReader();
+                //read the contents of the uploaded file and the evt sets the result and the new step
+                reader.readAsDataURL(files[0]);
+                reader.onload = evt => {
+                    this.image = evt.target.result;
+                    this.step = 2;
+                };
+                // Bypass for Chrome browser; enable reuploading of same files.
+                // Directly set the value of the input field to a blank string.
+                // Now when the user attempts to re-upload the same file again;
+                // it will always be detected as a change event.
+                document.querySelector("#file").value = "";
+            }
         },
         components: {
             Phonebody
